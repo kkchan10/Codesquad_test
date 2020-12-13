93e20c49423343df848c38a4f939e7e0edb2b2b2
DESKTOP-BR7SS91\Chan <kkchan9210@gmail.com>
2020-12-13 16:15:11 +0900

    Codesquad 3단계 문제 커밋용_3
    3단계: 루빅스 큐브 구현하기
    사용자 입력에 따라 해당하는 면의 행,열을 변경하는 메소드 구현
    
    변경 내용
    1) 프로그램 종료 시 조작 받은 명령어 갯수 출력 추가
    2) 사용자 입력에 따라 해당 면 회전시 인접 면의 회전 기능 추가

--- rubiks_cube.java
+++ rubiks_cube.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 class rubiks{
 	String[][][] cube = new String[6][3][3];
+	int count=0;
 	
 	void init() {
 		String[][][] cube = {{{"B0","B1","B2"},{"B3","B4","B5"},{"B6","B7","B8"}},{{"W0","W1","W2"},{"W3","W4","W5"},{"W6","W7","W8"}},{{"O0","O1","O2"},{"O3","O4","O5"},{"O6","O7","O8"}},{{"G0","G1","G2"},{"G3","G4","G5"},{"G6","G7","G8"}},{{"Y0","Y1","Y2"},{"Y3","Y4","Y5"},{"Y6","Y7","Y8"}},{{"R0","R1","R2"},{"R3","R4","R5"},{"R6","R7","R8"}}};
@@ -18,7 +19,6 @@ class rubiks{
 		String inpstr = "";
 		
 		for(int i=0;i<input_arr.length;i++) {
-			
 			if(input_arr[i].equals("Q")) {
 				System.out.println("Bye~");
 				break;
@@ -34,9 +34,173 @@ class rubiks{
 			}
 			order(inpstr);
 			print();
+			count++;
 		}
 	}
 
+	void order(String input) {
+		System.out.println(input);
+		if(input.equals("F")) {
+			F();
+		} else if(input.equals("F'")) {
+			rF();
+		}else if(input.equals("U")) {
+			U();
+		} else if(input.equals("U'")) {
+			rU();
+		} else if(input.equals("R")) {
+			R();
+		} else if(input.equals("R'")) {
+			rR();
+		} else if(input.equals("L")) {
+			L();
+		} else if(input.equals("L'")) {
+			rL();
+		} else if(input.equals("B")) {
+			B();
+		} else if(input.equals("B'")) {
+			rB();
+		} else if(input.equals("D")) {
+			D();
+		} else if(input.equals("D'")) {
+			rD();
+		}
+		
+	}
+
+	void F() {
+			Turn90(2);
+			for(int i=0;i<cube[2].length;i++) {
+				String temp = cube[0][2][i];
+				cube[0][2][i] = cube[1][cube[2].length-1-i][2];
+				cube[1][cube[2].length-1-i][2] = cube[5][0][cube[2].length-1-i];
+				cube[5][0][cube[2].length-1-i] = cube [3][i][0];
+				cube[3][i][0] = temp;
+		}
+	}
+	
+	void rF() {
+			rTurn90(2);
+			for(int i=0;i<cube[2].length;i++) {
+				String temp = cube[0][2][i];
+				cube[0][2][i] = cube[3][i][0];
+				cube[3][i][0] = cube[5][0][cube[2].length-1-i];
+				cube[5][0][cube[2].length-1-i] = cube[1][cube[2].length-1-i][2];
+				cube[1][cube[2].length-1-i][2] = temp;
+			}
+	}
+	
+	void U() {
+		Turn90(0);
+		for(int i=0;i<cube[0].length;i++) {
+			String temp = cube[1][0][i];
+			cube[1][0][i] = cube[2][0][i];
+			cube[2][0][i] = cube[3][0][i];
+			cube[3][0][i] = cube[4][0][i];
+			cube[4][0][i] = temp;
+			}
+	}
+	
+	void rU() {
+		rTurn90(0);
+		for(int i=0;i<cube[0].length;i++) {
+			String temp = cube[4][0][i];
+			cube[4][0][i] = cube[3][0][i];
+			cube[3][0][i] = cube[2][0][i];
+			cube[2][0][i] = cube[1][0][i];
+			cube[1][0][i] = temp;
+			}
+	}
+	
+	void R() {
+		Turn90(3);
+		for(int i=0;i<cube[3].length;i++) {
+			String temp = cube[0][i][2];
+			cube[0][i][2] = cube[2][i][2];
+			cube[2][i][2] = cube[5][i][2];
+			cube[5][i][2] = cube[4][cube[3].length-1-i][0];
+			cube[4][cube[3].length-1-i][0] = temp;
+			}
+	}
+	
+	void rR() {
+		rTurn90(3);
+		for(int i=0;i<cube[3].length;i++) {
+			String temp = cube[0][i][2];
+			cube[0][i][2] = cube[4][cube[3].length-1-i][0];
+			cube[4][cube[3].length-1-i][0] = cube[5][i][2];
+			cube[5][i][2] = cube[2][i][2];
+			cube[2][i][2] = temp;
+			}
+	}
+	
+	void L() {
+		Turn90(1);
+		for(int i=0;i<cube[1].length;i++) {
+			String temp = cube[0][i][0];
+			cube[0][i][0] = cube[4][cube[1].length-1-i][2];
+			cube[4][cube[1].length-1-i][2] = cube[5][i][0];
+			cube[5][i][0] = cube[2][i][0];
+			cube[2][i][0] = temp;
+			}		
+	}
+	
+	void rL() {
+		rTurn90(1);
+		for(int i=0;i<cube[1].length;i++) {
+			String temp = cube[0][i][0];
+			cube[0][i][0] = cube[2][i][0];
+			cube[2][i][0] = cube[5][i][0];
+			cube[5][i][0] = cube[4][cube[1].length-1-i][2];
+			cube[4][cube[1].length-1-i][2] = temp;
+			}		
+	}
+	
+	void B() {
+		Turn90(4);
+		for(int i=0;i<cube[4].length;i++) {	
+			String temp = cube[0][0][i];
+			cube[0][0][i] = cube[3][i][2];
+			cube[3][i][2] = cube[5][2][cube[4].length-1-i];
+			cube[5][2][cube[4].length-1-i] = cube[1][cube[4].length-1-i][0];
+			cube[1][cube[4].length-1-i][0] = temp;
+			
+			}
+	}
+	
+	void rB() {
+		rTurn90(4);
+		for(int i=0;i<cube[4].length;i++) {
+			String temp = cube[0][0][i];
+			cube[0][0][i] = cube[1][cube[4].length-1-i][0];
+			cube[1][cube[4].length-1-i][0] = cube[5][2][cube[4].length-1-i];
+			cube[5][2][cube[4].length-1-i] = cube[3][i][2];
+			cube[3][i][2] = temp;
+			}
+	}
+	
+	void D() {
+		Turn90(5);
+		for(int i=0;i<cube[5].length;i++) {
+			String temp = cube[1][2][i];
+			cube[1][2][i] = cube[4][2][i];
+			cube[4][2][i] = cube[3][2][i];
+			cube[3][2][i] = cube[2][2][i];
+			cube[2][2][i] = temp;
+			}
+	}
+	
+	void rD() {
+		rTurn90(5);
+		for(int i=0;i<cube[5].length;i++) {
+			String temp = cube[1][2][i];
+			cube[1][2][i] = cube[2][2][i];
+			cube[2][2][i] = cube[3][2][i];
+			cube[3][2][i] = cube[4][2][i];
+			cube[4][2][i] = temp;
+			}
+	}
+	
 	void Turn90(int side) {
 		int n = cube[side].length;
 		int m = cube[side][0].length;		
@@ -107,6 +271,7 @@ public class rubiks_cube {
 			if(input.contains("Q")) break;
 		}
 		scan.close();
+		System.out.println("Á¶ÀÛ °¹¼ö : "+ cube.count);
 	}
 }
 
